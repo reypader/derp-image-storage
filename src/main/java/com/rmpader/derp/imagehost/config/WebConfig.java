205dafbc524ff4f88193c7b66183f0a4e2899a37
reypader <reynaldmpader@gmail.com>
2015-03-08 14:35:11 +0800

    externalize file location and URL as properties

--- src/main/java/com/rmpader/derp/imagehost/config/WebConfig.java
+++ src/main/java/com/rmpader/derp/imagehost/config/WebConfig.java
@@ -1,22 +1,28 @@
 package com.rmpader.derp.imagehost.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.rmpader.derp.imagehost.property.ImageHostProperty;
+
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-	private static String resourceBaseURL = "/img/";
+    @Autowired
+    private ImageHostProperty   imageHostProperties;
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler(resourceBaseURL + "**")
-				.addResourceLocations("file:/home/reynald/public/");
-	}
+    private static final String resourceBaseURL = "/img/";
 
-	public static String getResourceBaseURL() {
-		return resourceBaseURL;
-	}
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(resourceBaseURL + "**").addResourceLocations(
+                "file:" + imageHostProperties.getUploadLocation());
+    }
+
+    public static String getResourceBaseURL() {
+        return resourceBaseURL;
+    }
 
 }
--- src/main/java/com/rmpader/derp/imagehost/controller/UploadController.java
+++ src/main/java/com/rmpader/derp/imagehost/controller/UploadController.java
@@ -5,7 +5,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.util.UUID;
 
-import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,51 +18,57 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.rmpader.derp.imagehost.config.WebConfig;
+import com.rmpader.derp.imagehost.property.ImageHostProperty;
 
 @RestController
 @RequestMapping(value = "/file")
 public class UploadController {
 
-	@RequestMapping(method = RequestMethod.POST)
-	public String handleFileUpload(@RequestParam("file") MultipartFile file) {
-		if (!file.isEmpty()) {
-			try {
+    @Autowired
+    private ImageHostProperty imageHostProperties;
 
-				byte[] bytes = file.getBytes();
-				String fileName = UUID.randomUUID().toString()
-						+ getFileExtension(file.getOriginalFilename());
-				String path = "/home/reynald/public/" + fileName;
-				BufferedOutputStream stream = new BufferedOutputStream(
-						new FileOutputStream(new File(path)));
-				stream.write(bytes);
-				stream.close();
-				String baseURL = ControllerLinkBuilder.linkTo(getClass())
-						.withSelfRel().getHref();
-				return baseURL + WebConfig.getResourceBaseURL() + fileName;
-			} catch (Exception e) {
-				return "Failed to upload => " + e.getMessage();
-			}
-		} else {
-			return "File was empty.";
-		}
-	}
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<String> handleFileUpload(HttpServletRequest request, @RequestParam("file") MultipartFile file) {
+        try {
+            if (!file.isEmpty()) {
+                byte[] bytes = file.getBytes();
+                String fileName = UUID.randomUUID().toString() + getFileExtension(file.getOriginalFilename());
+                String filePath = createFilePath(fileName);
 
-	@RequestMapping(value = "/{fileName:.+}", method = RequestMethod.DELETE)
-	public ResponseEntity<String> handleFileDelete(
-			@PathVariable("fileName") String fileName) {
-		String path = "/home/reynald/public/" + fileName;
-		File file = new File(path);
-		if (file.exists() && file.canWrite()) {
-			file.delete();
-			return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
-		} else {
-			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
-		}
-	}
+                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(filePath)));
+                stream.write(bytes);
+                stream.close();
 
-	private String getFileExtension(String fileName) {
-		String[] nameItems = fileName.split("\\.");
-		int lastElement = nameItems.length - 1;
-		return "." + nameItems[lastElement];
-	}
+                String imageURL = imageHostProperties.getBaseURL() + WebConfig.getResourceBaseURL() + fileName;
+
+                return new ResponseEntity<String>(imageURL, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getLocalizedMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private String createFilePath(String fileName) {
+        return imageHostProperties.getUploadLocation() + fileName;
+    }
+
+    @RequestMapping(value = "/{fileName:.+}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> handleFileDelete(@PathVariable("fileName") String fileName) {
+        String filePath = createFilePath(fileName);
+        File file = new File(filePath);
+        if (file.exists() && file.canWrite()) {
+            file.delete();
+            return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    private String getFileExtension(String fileName) {
+        String[] nameItems = fileName.split("\\.");
+        int lastElement = nameItems.length - 1;
+        return "." + nameItems[lastElement];
+    }
 }
